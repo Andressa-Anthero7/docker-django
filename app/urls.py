feb2d591fefd7dd355bc806aa09df30b19ffe3f7
Andressa-Anthero7 <andressa.anthero7@gmail.com>
2024-10-28 18:51:16 -0300

    pausa 28/10/20024

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('agradecimento/', views.agradecimento, name='agradicimento'),
+    path('accounts/login/<str:user>/dashboard/', views.dashboard, name='dashboard'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 import requests
+from .models import Leads
+from datetime import datetime
 
 
 from django.shortcuts import render
 import requests
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -11,11 +14,13 @@ def index(request):
         # Capturar os dados do formulário
         nome_leads = request.POST.get('nome') 
         whats_app_leads = request.POST.get('whatsapp')
+        recebido_em = datetime.now()
+        Leads.objects.create(nome_leads=nome_leads,whats_app_leads=whats_app_leads,data_recebimento=recebido_em)
 
         whats_app_receptor = "5516993379492"
         
         # Montar a mensagem
-        message = f"Olá, Adriana! Voçe recebeu novo leads- Nome:{nome_leads} - WhatsApp:{whats_app_leads}"
+        message = f"Olá, Adriana! Voçe recebeu novo leads- Nome:{nome_leads} - WhatsApp:{whats_app_leads}<br>Acesse:www.afunimesaocarlos.com.br/accounts/login/adriana/dashboard/"
 
         # Sua API Key fornecida pelo CallMeBot
         api_key = "1271569"  # Substitua pela sua API Key
@@ -29,13 +34,27 @@ def index(request):
         # Verificar a resposta
         if response.status_code == 200:
             print("Mensagem enviada com sucesso!")
-            return render(request, 'site/index.html')
+            return render(request, 'site/agradecimento.html')
+            print('passou oelo return')
         else:
             print("Falha ao enviar a mensagem.")
+            # precisa pop up para mostrar a falha de envio
+
             print(f"Status code: {response.status_code}")
     else:
         print("GET")
         return render(request, 'site/index.html')
+
+def agradecimento(request):
+    return render(request, 'site/agradecimento.html')
+
+@login_required
+def dashboard(request, user):
+    leads = Leads.objects.all().order_by('-data_recebimento')
+    return render(request,'site/dashboard.html',{'leads': leads})
+
+
+
 
     
 
