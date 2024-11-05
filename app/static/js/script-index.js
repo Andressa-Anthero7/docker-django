4d09d5508ad2d1ec3a146e17b484cdf5a6289a5b
Andressa-Anthero7 <andressa.anthero7@gmail.com>
2024-11-05 15:36:35 -0300

    correcao rolagem de tela mobile

--- app/static/js/script-index.js
+++ app/static/js/script-index.js
@@ -1,10 +1,6 @@
-$( document ).ready(function() {
-    // Quando o modal for fechado
-    $(document).on('click','.close', function () {
-        // Scroll até o formulário
-        $('html, body').animate({
-            scrollTop: $("#text-copy-lp").offset().top
-        }, 500); // Tempo em milissegundos para o scroll (0.5 segundo)
-    });
-
+$(document).on('click', '.close', function () {
+    $('body').css('overflow', 'auto');
+    $('html, body').animate({
+        scrollTop: $("#text-copy-lp").offset().top
+    }, 500);
 });
